241b6b3fc5c277f495ea0b5894fd1f4c2d2f2fc7
Tuuben <tobbe.helsing@gmail.com>
2021-09-25 09:35:32 +0300

    Initial Commit

---
+++ GraphicsPlayground/GraphicsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsPlaygroundApp.swift
+//  GraphicsPlayground
+//
+//  Created by Tobias Helsing on 25.9.2021.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
